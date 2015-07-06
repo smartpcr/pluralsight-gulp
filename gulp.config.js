6f49e3636da9fda8edb078a50126fa0bef13e672
xiaodongli <li.xiaodong@biosoftworks.com>
2015-07-05 23:19:29 -0400

    gulp watcher

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,17 @@
 module.exports = function() {
     "use strict";
+    var client = './src/client/';
+
     var config = {
+        temp: './.tmp/',
+
         // all js to vet
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+
+        less: client + 'styles/styles.less'
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var args = require('yargs').argv;
 var $ = require('gulp-load-plugins')({lazy: true});
 var config = require('./gulp.config')();
-
+var del = require('del');
 
 gulp.task('vet', function() {
     'use strict';
@@ -16,19 +16,55 @@ gulp.task('vet', function() {
         .pipe($.jshint.reporter('fail'));
 });
 
+gulp.task('styles', ['clean-styles'], function() {
+    '"use strict';
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        //.on('error', errorLogger)
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function(done) {
+    "use strict";
+    var files = config.temp + '**/*.css';
+    clean(files, done);
+});
+
+gulp.task('less-watcher', function() {
+    "use strict";
+    gulp.watch([config.less], ['styles']);
+});
+
 
 // util function
-function log(msg) {
-    'use strict';
 
+function clean(path, done) {
+    "use strict";
+    log('Cleaning files in ' + $.util.colors.blue(path));
+    del(path, done);
+}
+
+function log(msg) {
     if (typeof(msg) === 'object') {
         for (var item in msg) {
             if (msg.hasOwnProperty(item)) {
                 $.util.log($.util.colors.blue(msg[item]));
             }
         }
-    }
-    else {
+    } else {
         $.util.log($.util.colors.blue(msg));
     }
+}
+
+function errorLogger(error) {
+    "use strict";
+    log('*** Start of Error ***');
+    log(error);
+    log('*** End of Error ***');
+    this.emit('end');
 }
